828243e470da9dad09147db4503d5aa17f203775
rodrigo12xd <67241774+rodrigo12xd@users.noreply.github.com>
2025-03-28 20:33:09 -0300

    Update squidproxy.sh

--- squidproxy.sh
+++ squidproxy.sh
@@ -1,5 +1,7 @@
 apt update && apt install -y squid apache2-utils
-
+rm /etc/squid/squid.conf
+wget https://raw.githubusercontent.com/rodrigo12xd/proxy-socks5/refs/heads/master/squid.conf
+mv squid.conf /etc/squid/squid.conf
 
 sudo ufw allow 3128
 sudo systemctl restart squid
